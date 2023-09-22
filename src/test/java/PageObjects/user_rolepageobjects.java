4d0be7e87004573b77b71712ceb2fc90cb99f9b2
inter <inter@DESKTOP-92L7CGC>
2023-09-22 05:43:58 -0600

    Second Commit

--- src/test/java/PageObjects/user_rolepageobjects.java
+++ src/test/java/PageObjects/user_rolepageobjects.java
@@ -1,6 +1,13 @@
 package PageObjects;
 
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
 public class user_rolepageobjects {
+	
+	@FindBy(xpath="//*[@id=\"app\"]/div[1]/div[1]/aside/nav/div[2]/ul/li[1]/a")
+	
+	public static WebElement Admin;
 	
 
 }
