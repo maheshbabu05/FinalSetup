848c0d40384cf28f5daf028f57a8c265bd474541
inter <inter@DESKTOP-92L7CGC>
2023-09-25 03:28:34 -0600

    MOnday Commit

--- src/test/java/TestCase/pendingleaverequest.java
+++ src/test/java/TestCase/pendingleaverequest.java
@@ -1,12 +1,34 @@
 package TestCase;
 
+import org.openqa.selenium.interactions.Action;
 import org.testng.annotations.Test;
 
 import CommonPackage.CommonFunctions;
+import PageObjects.Login_Page;
+import PageObjects.user_rolepageobjects;
 
 public class pendingleaverequest extends CommonFunctions {
+	
+	
+	public void login() {
+		Login_Page.username.sendKeys("maheshbabu@captechin.com");
+		Login_Page.password.sendKeys("12345@Test");
+		Login_Page.submit.click();
+		
+	}
+	public void SearchAdminUser() {
+		
+		
+		user_rolepageobjects.admin.click();
+	}
+	
+	
 	@Test
 	public void verifyPendingLeaverequest() {
+		login();
+		
+		
+		
 		
 	}
 
